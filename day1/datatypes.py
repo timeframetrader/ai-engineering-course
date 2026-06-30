23b565c3b201fbd8af238645f1b380febc82f0d7
Abinash <abinashprusty1886@gmail.com>
2026-06-30 12:19:29 +0530

    Day 1: hello.py and datatypes.py - intro to variables and data types

---
+++ day1/datatypes.py
@@ -0,0 +1,12 @@
+city = "Rourkela"          # str
+population = 600000        # int
+avg_temp = 28.5             # float
+is_capital = False          # bool
+
+print(type(city))
+print(type(population))
+print(type(avg_temp))
+print(type(is_capital))
+
+sentence = city + " has a population of " + str(population) + " and an average temperature of " + str(avg_temp) + "C."
+print(sentence)
